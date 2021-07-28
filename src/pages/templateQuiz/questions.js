c7827d3e1f7982b4e07c5c4de1c2c1df70272c69
Pranaya Anargya <panargya@gmail.com>
2021-07-28 21:18:22 +1000

    changed the questions

--- src/pages/templateQuiz/questions.js
+++ src/pages/templateQuiz/questions.js
@@ -15,44 +15,44 @@ export const QuestionBank = [
         id: 2,
         question: "What is the name of Joe Rogan's Podcast?",
         options: {
-            option1: 'A Singer',
-            option2: 'A Actor',
-            option3: 'A UFC Fighter',
-            option4: 'A Podcast Host',
+            option1: 'The Joe Show',
+            option2: 'National Joegraphic',
+            option3: 'The Joe Rogan Experience',
+            option4: 'Ancient Hallucinogens with Joe',
         },
-        solution: 'A Podcast Host'
+        solution: 'The Joe Rogan Experience'
     },
     {
         id: 3,
         question: 'Joe Rogan has another career besides podcasting. What is it?',
         options: {
-            option1: 'A Singer',
-            option2: 'A Actor',
-            option3: 'A UFC Fighter',
-            option4: 'A Podcast Host',
+            option1: 'Chef',
+            option2: 'Pilot',
+            option3: 'UFC Commentator',
+            option4: 'Tae Kwon Do Instructor',
         },
-        solution: 'A Podcast Host'
+        solution: 'UFC Commentator'
     },
     {
         id: 4,
         question: "Which one of these guests did not appear in Joe Rogan's Podcast?",
         options: {
-            option1: 'A Singer',
-            option2: 'A Actor',
-            option3: 'A UFC Fighter',
-            option4: 'A Podcast Host',
+            option1: 'Kanye West',
+            option2: 'Robert Downey Jr.',
+            option3: 'Bernie Sanders',
+            option4: 'Roe Jogan',
         },
-        solution: 'A Podcast Host'
+        solution: 'Roe Jogan'
     },
     {
         id: 5,
-        question: 'What does DMT mean to Joe Rogan?',
+        question: 'Infamous Joe Rogan question?',
         options: {
-            option1: 'A Singer',
-            option2: 'A Actor',
-            option3: 'A UFC Fighter',
-            option4: 'A Podcast Host',
+            option1: '"Have you tried DMT?"',
+            option2: '"Why is the earth flat?',
+            option3: '"Have you tried elk liver?"',
+            option4: '"What do you know about the aliens?"',
         },
-        solution: 'A Podcast Host'
+        solution: '"Have you tried DMT?"'
     },
 ]
